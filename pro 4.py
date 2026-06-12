d32fc5109c6be5f30220d3646fe96db0c475b74f
pransu1719 <pransupatel452@gmail.com>
2026-06-12 18:31:18 +0530

    Update pro 4.py

--- pro 4.py
+++ pro 4.py
@@ -1,15 +1,27 @@
 print("Welcome to the Data Analyzer and Transformer Program")
-print("1. Input Data")
-print("2. Display Data Summary (Built-in Functions)")
-print("3. Calculate Factorial (Recursion)")
-print("4. Filter Data by Threshold (Lambda Function)")
-print("5. Sort Data")
-print("6. Display Dataset Statistics (Return Multiple Values)")
-print("7. Exit Program")
+print()
+def fact(n):
+  if n==0:
+      return 1
+  else:
+      return n*fact(n-1)
 
-# Step 1: Input Data
+while True:
+    print("select an option:")
+    print()
+    
+    print("1. Input Data")
+    print("2. Display Data Summary (Built-in Functions)")
+    print("3. Calculate Factorial (Recursion)")
+    print("4. Filter Data by Threshold (Lambda Function)")
+    print("5. Sort Data")
+    print("6. Display Dataset Statistics (Return Multiple Values)")
+    print("7. Exit Program")
+    print(
 
-choice = int(input("Please enter your choice: "))
+
+    choice = int(input("Please enter your choice: "))
+    print()
 
 if choice == 1:
     print("Step 1: Input Data")
